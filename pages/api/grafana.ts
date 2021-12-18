7fb173b561f39409aa81395ca9e87a4fb5f363d5
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2021-12-18 16:52:17 +0100

    feat: status 200

--- pages/api/grafana.ts
+++ pages/api/grafana.ts
@@ -11,7 +11,7 @@ export default function handler(
 ) {
   console.log(req.body);
   if (req.body.state === "alerting") {
-    res.status(500).json(req.body);
+    res.status(200).json(req.body);
   } else {
     res.status(200).json(req.body);
   }
